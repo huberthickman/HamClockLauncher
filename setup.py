7529904669b40e6a4709d845f56b5a4f17990f87
Hubert Hickman <hubert.hickman@gmail.com>
2026-08-29 23:00:35 -0500

    Updating to 4.31 (#13)

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import sys
 import datetime
 import fileinput
 
-version_string = "4.30"
+version_string = "4.31"
 
 #
 # Modify the build_info text file to have the current build date, number and correct version
@@ -35,7 +35,7 @@ if sys.platform == 'darwin':
     Plist = dict(
         CFBundleName='HamClockLauncher',
         CFBundleGetInfoString='HamClockLauncher',
-        CFBundleShortVersionString='4.30',
+        CFBundleShortVersionString='4.31',
         NSHumanReadableCopyright='HamClockLauncher © 2025-2026, Hubert Hickman'
         )
 
